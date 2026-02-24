5f3143568ee032ee4c4ace6a60aa3b50f028d293
Juan Cardenas Silva <juanpoodres@gmail.com>
2026-02-24 12:22:14 -0500

    Primer commit - Agrega clase ListaCodigos

---
+++ src/Conversores/ListaCodigos.java
@@ -0,0 +1,13 @@
+package Conversores;
+
+import java.util.List;
+
+public class ListaCodigos {
+
+        private static final List<String> ordenDeseado = List.of("ARS", "USD", "BRL", "EUR", "GBP", "JPY");
+
+        public static List<String> obtenerCodigos(){
+            return ordenDeseado;
+        }
+
+}
